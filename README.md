17435977df133921bc5578ea709e3e6107e5eaeb
SimonPower <liangshaomin20@163.com>
2021-08-25 11:45:08 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,13 @@
+# uniapp-project-template
+
+## 介绍
+
+uniapp vue-cli 模式的基本模板，方便新建项目时不用安装配置，包含了以下插件：
+
+- [x] vue2
+- [x] typescript
+- [x] Eslint
+- [x] Prettier
+- [x] lint-staged
+- [x] husky
+- [x] commitlint
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <view class="content"> </view>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class App extends Vue {
+  mpType = 'app'
+
+  onLaunch(): void {
+    console.log('App Launch')
+  }
+
+  onShow(): void {
+    console.log('App Show')
+  }
+
+  onHide(): void {
+    console.log('App Hide')
+  }
+}
+</script>
+
+<style>
+/*每个页面公共css */
+</style>
